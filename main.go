c27cd3bff8d9a805e9502884264d85e54291bf3c
matwe <matweewilya@gmail.com>
2022-08-11 17:44:16 +0300

    first commit of tg_bot

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"os"
+)
+
+var AnswerKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Да"),
+		tgbotapi.NewKeyboardButton("Нет"),
+	))
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	// Loop through each update.
+	for update := range updates {
+		switch update.Message.Text { // ignore any non-Message updates
+		case "Да":
+
+		case "Нет":
+
+		default:
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я ещё глупенький бот и понимаю только команды(\nМожешь написать /help, чтобы узнать о моих функциях")
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "start":
+			msg.Text = ""
+		default:
+			msg.Text = "Я пока не знаю такой команды"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
